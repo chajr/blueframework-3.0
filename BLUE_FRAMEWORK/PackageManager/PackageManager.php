49cd9d4e6cca138617d047151f3893f1f5c12a97
Michal Adamiak <chajr@bluetree.pl>
2013-04-25 19:53:28 +0200

    Some basic files development progress
    This version introduces multiple enhancements:
    
    Created base content for .htaccess, index.php and PackageManager.php.
    Adde new files Upgrade and Install
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   /.htaccess
                -- Updated --
                    added redirecting all requests to index.php
    
    Modified:   /BLUE_FRAMEWORK/.htaccess
                -- Updated --
                   deny for all request
    
    Modified:   /index.php
                -- Updated --
                    Loading PakageManager class, runing method that sets base paths
    
    Modified:   /BLUE_FRAMEWORK/PackageManager/PackageManager.php
                -- Added --
                    setPaths method, to set base framework paths as constains
                    loadLibrary method to load libraries packages
                    loadModule method to load module packages
                    name2path method to convert library or module name to path
                    some variables to contains list of loaded, missing or disabled modules/libraries
    
    Added:      /BLUE_FRAMEWORK/PackageManager/Upgrade.php
                -- Added --
                    file for upgrade module/library class
    
    Added:      /BLUE_FRAMEWORK/PackageManager/Install.php
                -- Added --
                    file for install module/library class

--- BLUE_FRAMEWORK/PackageManager/PackageManager.php
+++ BLUE_FRAMEWORK/PackageManager/PackageManager.php
@@ -1,12 +1,76 @@
 <?php
 /**
  * @author chajr <chajr@bluetree.pl>
- * @version 0.1.0
+ * @version 0.2.0
  * @copyright chajr/bluetree
  * @package BlueFramework3.0
  */
 
 class PackageManager
 {
-    
+    private static $_loadedLibraries    = array();
+    private static $_loadedModules      = array();
+    private static $_missingLibraries   = array();
+    private static $_missingModules     = array();
+    private static $_disabledLibraries  = array();
+    private static $_disabledModules    = array();
+    /**
+     * set constants with main path to index.php, framework files etc
+     * basically as argument use __FILE__
+     * @param string $indexFilePath base framework path
+     */
+    static function setPaths($indexFilePath)
+    {
+        $main = dirname($indexFilePath);
+        define('CORE_MAIN_PATH',             $main);
+        define('CORE_LOG_PATH',              $main . '/BLUE_FRAMEWORK/log/');
+        define('CORE_MAIN_FRAMEWORK_PATH',   $main . '/BLUE_FRAMEWORK/');
+        define('CORE_LIBRARIES_PATH',        $main . '/BLUE_FRAMEWORK/LIBRARIES/');
+        define('CORE_MODULES_PATH',          $main . '/BLUE_FRAMEWORK/MODULES/');
+        define('CORE_SKIN_PATH',             $main . '/BLUE_FRAMEWORK/SKIN/');
+        define('CORE_CONFIG_PATH',           $main . '/BLUE_FRAMEWORK/cfg/');
+        define('CORE_PATH',                  $main . '/BLUE_FRAMEWORK/LIBRARIES/Core/');
+        define('CORE_TEMP',                  $main . '/BLUE_FRAMEWORK/temp/');
+    }
+
+    /**
+     * @param string $librariesPackage
+     */
+    static function loadLibrary($librariesPackage)
+    {
+        if (class_exists('Test_Tracer')) {
+            Test_Tracer::marker(array(
+                'loading package', debug_backtrace()
+            ));
+        }
+        if (is_string($librariesPackage)) {
+            $librariesPackage = array($librariesPackage);
+        }
+        foreach ($librariesPackage as $library) {
+            
+        }
+        
+    }
+
+    /**
+     * 
+     */
+    static function loadModule()
+    {
+
+    }
+    /**
+     * convert class name to path
+     * @param string $packageName
+     * @param boolean $phpExtension
+     * @return string
+     */
+    static function name2path($packageName, $phpExtension = FALSE)
+    {
+        $path = str_replace('_', '/', $packageName);
+        if ($phpExtension) {
+            $path .= '.php';
+        }
+        return $path;
+    }
 }
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * @author chajr <chajr@bluetree.pl>
- * @version 0.1.0
+ * @version 0.2.0
  * @copyright chajr/bluetree
  * @package BlueFramework3.0
  */
+require_once 'BLUE_FRAMEWORK/PackageManager/PackageManager.php';
+PackageManager::setPaths(__FILE__);
